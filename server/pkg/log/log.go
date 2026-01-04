de57b917ca7a8e9b1b73d185019bed32d2e0a3a5
Kristina Pianykh <kristinavrnrus@gmail.com>
2026-01-04 18:52:07 +0100

    add log pkg

---
+++ server/pkg/log/log.go
@@ -0,0 +1,24 @@
+package log
+
+import (
+	"log"
+	"os"
+)
+
+func debugEnabled() bool {
+	return os.Getenv("LOG_LEVEL") == "debug"
+}
+
+func Debug(msg string, args ...any) {
+	if debugEnabled() {
+		log.Printf("[DEBUG] "+msg, args...)
+	}
+}
+
+func Info(msg string, args ...any) {
+	log.Printf("[INFO] "+msg, args...)
+}
+
+func Error(msg string, args ...any) {
+	log.Printf("[ERROR] "+msg, args...)
+}
