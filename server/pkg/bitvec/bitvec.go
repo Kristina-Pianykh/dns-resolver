6cd5a29ad0eb2cba42661ae049f8b321a921324e
Kristina Pianykh <kristinavrnrus@gmail.com>
2026-01-04 18:52:43 +0100

    create data structure for bit vector

---
+++ server/pkg/bitvec/bitvec.go
@@ -0,0 +1,90 @@
+package bitvec
+
+import (
+	"errors"
+	"fmt"
+
+	"server/pkg/log"
+)
+
+const MaxLength = 512
+
+type BitVec struct {
+	data       []byte
+	pos        int
+	bitOffset  int
+	byteOffset int
+}
+
+func NewBitVec(data []byte) (BitVec, error) {
+	if len(data) > MaxLength {
+		return BitVec{}, fmt.Errorf("input data is bigger than %d bytes", MaxLength)
+	}
+	d := make([]byte, 0, 512)
+	copy(d, data)
+	return BitVec{data: data}, nil
+}
+
+func (v *BitVec) updateOffsets() {
+	v.bitOffset = v.pos % 8
+	v.byteOffset = v.pos / 8
+}
+
+// 0b0100_0000
+func (v *BitVec) ReadBits(n int) (uint8, error) {
+	if n < 0 || n > 7 {
+		return 0, fmt.Errorf("Bit offset into byte must be between 0 and 7, got %d", n)
+	}
+
+	if v.bitOffset+n >= 8 {
+		return 0, fmt.Errorf("violated byte boundary when reading %d bits", n)
+	}
+	shift := 8 - v.bitOffset - n             // 6
+	shifted := v.data[v.byteOffset] >> shift // 1 in decimal
+	log.Debug("shift: %d\n", shift)
+	log.Debug("shifted: %08b\n", shifted)
+	log.Debug("255 >> (8 - %d): %b\n", n, 255>>(8-n))
+	masked := shifted & (255 >> (8 - n))
+	log.Debug("masked: %08b\n", masked)
+	v.pos += n
+	v.updateOffsets()
+	return masked, nil
+}
+
+func (v *BitVec) ReadBytes(n int) (uint32, error) {
+	if n < 0 {
+		return 0, fmt.Errorf("Can't read %d bytes", n)
+	}
+
+	if v.byteOffset >= 512 {
+		// TODO: signal nothing to do, reach end of the packet
+		return 0, errors.New("Reached the end of the packet")
+	}
+
+	if n+v.byteOffset > MaxLength || n+v.byteOffset > len(v.data) {
+		return 0, fmt.Errorf("Can't read %d + %d > %d", v.byteOffset, n, MaxLength)
+	}
+
+	// byte-long fields align with byte boundaries so bitOffset
+	// cannot be > 0
+	if v.bitOffset != 0 {
+		return 0, errors.New("Violated byte boundery when reading 2 bytes. Exiting.")
+	}
+	b := v.data[v.byteOffset : v.byteOffset+n]
+	v.pos += 8 * n
+	v.updateOffsets()
+
+	// Examples
+	// b[0]<<8 | b[1]
+	// b[0]<<24 | b[1]<<16 | b[2]<<8 | b[3]
+
+	var val uint32
+	for i := 0; i < n; i++ {
+		shift := (n - i - 1) * 8
+		log.Debug("i: %d, shift: %d\n", i, shift)
+		log.Debug("uint32(b[i])<<shift: %08b\n", uint32(b[i])<<shift)
+		val = val | uint32(b[i])<<shift
+	}
+	log.Debug("val: %08b\n", val)
+	return val, nil
+}
---
+++ server/pkg/bitvec/bitvec_test.go
@@ -0,0 +1,143 @@
+package bitvec
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadBits(t *testing.T) {
+	tests := []struct {
+		offset int
+		data   []byte
+		nBits  int
+		exp    int
+	}{
+		{
+			data:   []byte{0b0100_0000},
+			nBits:  1,
+			offset: 1,
+			exp:    1,
+		},
+		{
+			data:   []byte{0b0001_0000},
+			nBits:  1,
+			offset: 3,
+			exp:    1,
+		},
+		{
+			data:   []byte{0b0001_1000},
+			nBits:  2,
+			offset: 3,
+			exp:    3,
+		},
+		{
+			data:   []byte{0b0000_1000},
+			nBits:  2,
+			offset: 4,
+			exp:    2,
+		},
+		{
+			data:   []byte{0b0011_1000},
+			nBits:  3,
+			offset: 2,
+			exp:    7,
+		},
+		{
+			data:   []byte{0b0000_1100},
+			nBits:  3,
+			offset: 3,
+			exp:    3,
+		},
+		{
+			data:   []byte{0b0111_0100},
+			nBits:  4,
+			offset: 1,
+			exp:    14,
+		},
+		{
+			data:   []byte{0b0111_0100},
+			nBits:  4,
+			offset: 2,
+			exp:    0b1101,
+		},
+	}
+	for _, tt := range tests {
+		vec, err := NewBitVec(tt.data)
+		assert.NoError(t, err)
+		vec.bitOffset = tt.offset
+		res, err := vec.ReadBits(tt.nBits)
+		assert.NoError(t, err)
+		assert.Equal(t, uint8(tt.exp), res)
+	}
+}
+
+func TestReadBytes(t *testing.T) {
+	tests := []struct {
+		offset int
+		data   []byte
+		nBytes int
+		exp    int
+	}{
+		{
+			data:   []byte{0b0100_0000, 0b0111_0000, 0b1101_1101},
+			nBytes: 1,
+			offset: 1,
+			exp:    0b0111_0000,
+		},
+		{
+			data:   []byte{0b0100_0000, 0b0111_0000, 0b1101_1101},
+			nBytes: 2,
+			offset: 1,
+			exp:    0b0111_0000_1101_1101,
+		},
+		{
+			data:   []byte{0b0100_0000, 0b0111_0000, 0b1101_1101},
+			nBytes: 2,
+			offset: 0,
+			exp:    0b0100_0000_0111_0000,
+		},
+		{
+			data:   []byte{0b0100_0000, 0b0111_0000, 0b1101_1101},
+			nBytes: 1,
+			offset: 2,
+			exp:    0b1101_1101,
+		},
+		{
+			data:   []byte{0b0100_0000, 0b0111_0000, 0b1101_1101, 0b0},
+			nBytes: 3,
+			offset: 1,
+			exp:    0b0111_0000_1101_1101_0000_0000,
+		},
+	}
+	for _, tt := range tests {
+		vec, err := NewBitVec(tt.data)
+		assert.NoError(t, err)
+		vec.byteOffset = tt.offset
+		res, err := vec.ReadBytes(tt.nBytes)
+		assert.NoError(t, err)
+		assert.Equal(t, uint32(tt.exp), res)
+	}
+}
+
+func TestReadingInvalidNumberOfBits(t *testing.T) {
+	data := []byte{0b0}
+	vec, err := NewBitVec(data)
+	assert.NoError(t, err)
+	vec.bitOffset = 6
+
+	res, err := vec.ReadBits(2)
+	assert.Error(t, err)
+	assert.Equal(t, uint8(0), res)
+}
+
+func TestReadingInvalidNumberOfBytes(t *testing.T) {
+	data := []byte{0b0100_0000, 0b0111_0000}
+	vec, err := NewBitVec(data)
+	assert.NoError(t, err)
+	vec.byteOffset = 1
+
+	res, err := vec.ReadBytes(2)
+	assert.Error(t, err)
+	assert.Equal(t, uint32(0), res)
+}
