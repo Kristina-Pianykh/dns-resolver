a37b3a8020be0f833275f688dbbc45044f060d3d
Kristina Pianykh <kristinavrnrus@gmail.com>
2026-02-09 23:07:23 +0100

    fix: close UDP client connection when done

--- pkg/server/test_client.go
+++ pkg/server/test_client.go
@@ -1,13 +1,18 @@
 package server
 
-import "net"
+import (
+	"fmt"
+	"net"
+	"time"
+)
 
 type UDPClient struct {
-	Conn *net.UDPConn
-	Name string
+	Conn    *net.UDPConn
+	Name    string
+	Timeout time.Duration
 }
 
-func NewUDPClient(serverAddr string) (*UDPClient, error) {
+func NewUDPClient(serverAddr string, timeout time.Duration) (*UDPClient, error) {
 	udpAddr, err := net.ResolveUDPAddr("udp", serverAddr)
 	if err != nil {
 		return nil, err
@@ -32,15 +37,24 @@ func (c *UDPClient) Send(data []byte) (int, error) {
 func (c *UDPClient) SendAndReceive(data []byte, bufferSize int) ([]byte, error) {
 	buffer := make([]byte, bufferSize)
 
+	// fail fast instead of hanging forever if no response arrives
+	_ = c.Conn.SetReadDeadline(time.Now().Add(c.Timeout))
+
 	_, err := c.Conn.Write(data)
 	if err != nil {
 		return nil, err
 	}
+	fmt.Printf("sent %s\n", string(data))
 
 	n, _, err := c.Conn.ReadFromUDP(buffer)
 	if err != nil {
 		return nil, err
 	}
+	fmt.Printf("read %d bytes\n", n)
 
 	return buffer[:n], nil
+}
+
+func (c *UDPClient) Close() error {
+	return c.Conn.Close()
 }
